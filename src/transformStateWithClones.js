aa90e33b55dacdc9bb9ff5dd7e8da7df9ae2de3b
Dmytro Selikhov <dmytro.selikhov@gmail.com>
2025-02-21 12:54:01 +0400

    updated Solution transformStateWithClones

--- src/transformStateWithClones.js
+++ src/transformStateWithClones.js
@@ -23,6 +23,9 @@ function transformStateWithClones(state, actions) {
       case 'clear':
         currentState = {};
         break;
+
+      default:
+        throw new Error(`Unknown action type: ${action.type}`);
     }
 
     result.push({ ...currentState });
